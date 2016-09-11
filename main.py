6d1dd982c0f7036cff415a4e58b95398d5169be9
Brian Gunter <brigun@gmail.com>
2016-09-11 13:53:10 -0500

    Added new post form error handling, re-renders form with error message

--- main.py
+++ main.py
@@ -14,11 +14,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 import webapp2
+import os
+import jinja2
+
+from google.appengine.ext import db
+
+# set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Blog(db.Model):
+    title = db.StringProperty(required=True)
+    entry = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        error = self.request.get("error")
+        title = self.request.get("title")
+        entry = self.request.get("entry")
+
+        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created LIMIT 5")
+
+        t = jinja_env.get_template("add_form.html")
+        response = t.render(
+                        blogs = blogs,
+                        error = error)
+        self.response.write(response)
+
+    def post(self):
+
+        error = self.request.get("error")
+        title = self.request.get("title")
+        entry = self.request.get("entry")
+
+        if title and entry:
+
+            b = Blog(title = title, entry = entry)
+            b.put()
+            self.redirect("/")
+
+        else:
+
+            error = "We need both a title and an entry to create a new blog post."
+
+            self.redirect("/?error=" + error)
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
